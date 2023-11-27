21838354c58b0cbfc367a0f49002f14de2d110ef
Lamin Jawla <laminjawla1001@gmail.com>
2023-11-27 19:50:08 +0000

    8. Post-order traversal

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - Traverse the tree using
+ * postorder method
+ *
+ * @tree: The tree to be traversed
+ * @func: Function pointer
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
