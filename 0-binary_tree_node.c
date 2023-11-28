96aa08705b4a6a0222ae97f6430bad5d92ce9fcb
Lamin Jawla <laminjawla1001@gmail.com>
2023-11-28 08:09:29 +0000

    o

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * create_node - Creates a new node
- * and sets it field properties
- *
- * @value: Field value
- *
- * Return: A new node
- */
-binary_tree_t *create_node(int value)
-{
-	binary_tree_t *node = malloc(sizeof(binary_tree_t));
-
-	if (node == NULL)
-		return (NULL);
-	node->left = node->right = node->parent = NULL;
-	node->n = value;
-	return (node);
-}
-/**
  * binary_tree_node - Creates a binary tree node
  *
  * @parent: The parent node
